4e422114d72692418c095755e524a036bdd8f0a0
Trin1t4 <dario.tripol@gmail.com>
2025-11-12 12:36:07 +0100

    Use level from frontend instead of recalculating

--- api/program/generate.js
+++ api/program/generate.js
@@ -81,7 +81,7 @@ export default async function handler(req, res) {
       location: onboardingData.trainingLocation || 'gym',
       equipment: onboardingData.equipment || {},
       goal: onboardingData.goal || 'muscle_gain',
-      level: intelligentLevel.finalLevel,
+      level: userLevel || intelligentLevel.finalLevel,
       frequency: onboardingData.activityLevel?.weeklyFrequency || 3,
       painAreas: onboardingData.painAreas || [],
       disabilityType: onboardingData.disabilityType || null,
